b53c827642e78e5075afdb4b64bc3b68b900b78d
Marziah <marziah@newgen-bd.com>
2025-05-04 11:13:58 +0600

    config update commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,19 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/skillshopper-chatbot/", // <-- this is important
+  base: "/brl-chatbot/", // <-- this is important
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/index.js",
+        chunkFileNames: "assets/index.js",
+        assetFileNames: ({ name }) => {
+          if (name && name.endsWith(".css")) {
+            return "assets/index.css"; // Keep CSS in css folder
+          }
+          return "assets/[name].[ext]";
+        },
+      },
+    },
+  },
 });
